ca0e4f2950e07a22c64df9d45b96bb8df0f6e92c
Muzamil-Fatima <muzamilzubair987654@gmail.com>
2026-01-08 21:38:21 +0500

    Stop tracking dashboard folder

--- dashboard/src/pages/appointments/GridAppointment.jsx
+++
@@ -1,8 +0,0 @@
-const GridAppointment = () => {
-  return (
-    <>
-      <h2>The grid of appointment is here........</h2>
-    </>
-  );
-};
-export default GridAppointment;
--- dashboard/src/pages/appointments/PendingAppointment.jsx
+++
@@ -1,8 +0,0 @@
-const PendingAppointment = () => {
-  return (
-    <>
-      <div>h2 All pending appointment is here </div>
-    </>
-  );
-};
-export default PendingAppointment;
